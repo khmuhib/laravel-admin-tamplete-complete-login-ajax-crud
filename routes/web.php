4338ded5787dee47122d48c97be2ffd08d82b7dc
kh muhib <khmuhib2013@gmail.com>
2022-10-13 18:11:10 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Http\Controllers\AkunController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    //return view('welcome');
+    return redirect('login');
+});
+
+Auth::routes();
+
+Route::group(['prefix' => 'dashboard/admin'], function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+
+    Route::group(['prefix' => 'profile'], function () {
+        Route::get('/', [HomeController::class, 'profile'])->name('profile');
+        Route::post('update', [HomeController::class, 'updateprofile'])->name('profile.update');
+    });
+
+    Route::controller(AkunController::class)
+        ->prefix('akun')
+        ->as('akun.')
+        ->group(function () {
+            Route::get('/', 'index')->name('index');
+            Route::post('showdata', 'dataTable')->name('dataTable');
+            Route::match(['get','post'],'tambah', 'tambahAkun')->name('add');
+            Route::match(['get','post'],'{id}/ubah', 'ubahAkun')->name('edit');
+            Route::delete('{id}/hapus', 'hapusAkun')->name('delete');
+        });
+});
